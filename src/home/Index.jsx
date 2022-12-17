76141ca4dcaffc3b16d9cbef09ad73002c78a713
claudioares <claudiojasoares.dev@gmail.com>
2022-12-17 06:19:26 -0300

    Inicializado configurações. Projeto pronto pra codigo

---
+++ src/home/Index.jsx
@@ -0,0 +1,14 @@
+import {
+  StyledHome
+} from './styled';
+
+function Home() {
+
+  return (
+    <StyledHome>
+      Ola, meu projeto!
+    </StyledHome>
+  )
+}
+
+export default Home;
---
+++ src/home/styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+export const StyledHome = styled.div`
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: black;
+    color: wheat;
+`
